3b38a34ae680d4bf950232377cd9c50e0727c55d
champa <champabhagavathi@gmail.com>
2022-04-07 15:11:43 -0400

    remove level filter and add network filter

--- secChargeAngularWebClient/src/app/components/display-maps-for-view-stations/display-maps.component.ts
+++ secChargeAngularWebClient/src/app/components/display-maps-for-view-stations/display-maps.component.ts
@@ -26,30 +26,21 @@ export class DisplayMapsComponent implements OnInit {
   coordsFromNearestStationService: StationObj[] = [];
   markerIcon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
   //"{icon:{url:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}}"
-  levelSelectEventSubscription!: Subscription;
+  
   connectorSelectEventSubscription!: Subscription;
   networkSelectEventSubscription!: Subscription;
 
-  levelsFilterStations: StationObj[] = [];
+  
   connectorFilterStations: StationObj[] = [];
   networkFilterStations: StationObj[] = [];
 
   coordsFromConnectorFilterFormSearchLoc: StationObj[] = [];
   curStateOfConnectorFilterFromSearchLoc: string[] = [];
 
-  curStateOfLevelFilterFromSearchLoc: string[] = [];
-  coordsFromLevelFilterFormSearchLoc: StationObj[] = [];
+  coordsFromNetworkFilterFormSearchLoc: StationObj[] = [];
+  curStateOfNetworkFilterFromSearchLoc: string[] = [];
 
   dropdownSettings!: IDropdownSettings;
-  levelDropdownList = [
-    { item_id: 1, item_text: 'Level 1' },
-    { item_id: 2, item_text: 'Level 2' },
-    { item_id: 3, item_text: 'DC Fast' },
-    { item_id: 4, item_text: 'Legacy Chargers' }
-  ];
-  levelSelectedItems = [
-
-  ];
 
   connectorDropdownList = [
     { item_id: 1, item_text: 'NEMA1450' },
@@ -62,6 +53,46 @@ export class DisplayMapsComponent implements OnInit {
 
   ];
   connectorSelectedItems = [
+
+  ];
+
+  networkDropdownList = [
+    { item_id: 2, item_text: 'AddÉnergie Technologies' },
+    { item_id: 3, item_text: 'AMPUP' },
+    { item_id: 4, item_text: 'BCHYDRO' },
+    { item_id: 5, item_text: 'Blink Network' },
+    { item_id: 6, item_text: 'CHARGELAB' },
+    { item_id: 7, item_text: 'ChargePoint Network' },
+    { item_id: 8, item_text: 'Circuit électrique' },
+    { item_id: 9, item_text: 'eCharge Network' },
+    { item_id: 10, item_text: 'Electrify Canada' },
+    { item_id: 11, item_text: 'EVCS' },
+    { item_id: 12, item_text: 'EV Connect' },
+    { item_id: 13, item_text: 'EVGATEWAY' },
+    { item_id: 14, item_text: 'eVgo Network' },
+    { item_id: 15, item_text: 'FLO' },
+    { item_id: 16, item_text: 'FPLEV' },
+    { item_id: 17, item_text: 'FCN' },
+    { item_id: 18, item_text: 'Greenlots' },
+    { item_id: 19, item_text: 'IVY' },
+    { item_id: 20, item_text: 'LIVINGSTON' },
+    { item_id: 22, item_text: 'Non-Networked' },
+    { item_id: 23, item_text: 'OpConnect' },
+    { item_id: 24, item_text: 'PETROCAN' },
+    { item_id: 25, item_text: 'POWERFLEX' },
+    { item_id: 26, item_text: 'RIVIAN_WAYPOINTS' },
+    { item_id: 27, item_text: 'SemaCharge Network' },
+    { item_id: 28, item_text: 'Sun Country Highway' },
+    { item_id: 29, item_text: 'Tesla Destination' },
+    { item_id: 30, item_text: 'SWTCH' },
+    { item_id: 31, item_text: 'Tesla' },
+    { item_id: 32, item_text: 'Volta' },
+    { item_id: 33, item_text: 'Webasto' },
+    { item_id: 34, item_text: 'ZEFNET' }
+
+
+  ];
+  networkSelectedItems = [
 
   ];
 
@@ -99,13 +130,6 @@ export class DisplayMapsComponent implements OnInit {
 
   constructor(private sharedService: SharedService, private getNearestLocationInfoService: GetLocationBasedStationInfoService) {
 
-    this.levelSelectEventSubscription = this.sharedService.getLevelSelectEvent().subscribe((value: StationObj[]) => {
-      value.forEach(ele => console.log(ele))
-      this.levelDataRefresh(value);
-    });
-    console.log('LevelFilter:')
-    console.log(this.levelsFilterStations);
-
     this.connectorSelectEventSubscription = this.sharedService.getConnectorSelectEvent().subscribe((value: StationObj[]) => {
       this.connectorDataRefresh(value);
     });
@@ -131,12 +155,6 @@ export class DisplayMapsComponent implements OnInit {
     };
   }
 
-  levelDataRefresh(value: StationObj[]) {
-
-    this.levelsFilterStations = value;
-    this.stationsToBeMarked = this.levelsFilterStations;
-  }
-
   connectorDataRefresh(value: StationObj[]) {
     this.connectorFilterStations = value;
     console.log(this.connectorFilterStations)
@@ -151,7 +169,7 @@ export class DisplayMapsComponent implements OnInit {
 
 
   ngOnDestroy() {
-    this.levelSelectEventSubscription.unsubscribe();
+
     this.connectorSelectEventSubscription.unsubscribe();
     this.networkSelectEventSubscription.unsubscribe();
   }
@@ -313,6 +331,127 @@ export class DisplayMapsComponent implements OnInit {
     );
     console.log(items);
   }
+  onNetworkDeSelect(item: any) {
+
+    let deSelectNetworkUrl = '&ev_network=';
+    deSelectNetworkUrl = deSelectNetworkUrl.concat(item.item_text);
+    deSelectNetworkUrl = this.locSearched.concat(deSelectNetworkUrl);
+    console.log(deSelectNetworkUrl)
+
+    this.curStateOfNetworkFilterFromSearchLoc = this.curStateOfNetworkFilterFromSearchLoc.filter(e => e !== item.item_text)
+
+    if (!(this.curStateOfNetworkFilterFromSearchLoc.length == 0)) {
+
+      console.log('String to be passed:Getting the coords to be removed ' + deSelectNetworkUrl);
+
+      this.getNearestLocationInfoService.getStationInfoFromSearchedLoc(deSelectNetworkUrl).subscribe((info) => {
+        this.response = info;
+
+        this.response.fuel_stations.forEach((station: FuelStationInfo) => {
+
+          console.log('Station Name:', station.station_name,
+            '**Station network:', station.ev_network,
+            '**Station connector type:', station.ev_connector_types,
+            '**Latitude', station.latitude,
+            '**Longitude', station.longitude, '\n');
+
+          let coord: Coordinate = { lat: station.latitude, lng: station.longitude };
+
+          console.log('Cood to be eliminated ');
+          console.log(coord)
+
+          let i = -1;
+          this.coordsFromNetworkFilterFormSearchLoc.forEach(ord => {
+
+            i++;
+            if (ord.stationLocCoord.lat == coord.lat && ord.stationLocCoord.lng == coord.lng) {
+              console.log(ord);
+              this.coordsFromNetworkFilterFormSearchLoc.splice(i, 1);
+            }
+
+          });
+          console.log('Final list after removal');
+          console.log(this.coordsFromNetworkFilterFormSearchLoc)
+        }
+        )
+      });
+      this.sharedService.sendConnectorSelect(this.coordsFromNetworkFilterFormSearchLoc);
+    }
+    else {
+      this.clearMapForNetworkFilter()
+    }
+  }
+
+  onNetworkDeSelectAll(items: any) {
+    this.clearMapForNetworkFilter()
+  }
+
+  clearMapForConnectorFilter() {
+    this.coordsFromConnectorFilterFormSearchLoc = [];
+    this.curStateOfConnectorFilterFromSearchLoc = [];
+    this.sharedService.sendConnectorSelect(this.coordsFromConnectorFilterFormSearchLoc);
+
+  }
+
+
+  clearMapForNetworkFilter() {
+    this.coordsFromNetworkFilterFormSearchLoc = [];
+    this.curStateOfNetworkFilterFromSearchLoc = [];
+    this.sharedService.sendConnectorSelect(this.coordsFromNetworkFilterFormSearchLoc);
+
+  }
+
+  setMarkerIcon(statObj: StationObj): string {
+
+    if (statObj.stationL2Num != null) statObj.color = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+    else if (statObj.stationL1Num != null) statObj.color = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    else if (statObj.stationDCNum != null) statObj.color = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+    else statObj.color = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+
+    return statObj.color;
+  }
+
+  onNetworkSelect(item: any) {
+  
+    this.curStateOfNetworkFilterFromSearchLoc.push(item.item_text);
+    let selectedNetworkUrl = this.locSearched.concat('&ev_network=');
+    console.log(selectedNetworkUrl)
+
+    for (let network of this.curStateOfNetworkFilterFromSearchLoc) {
+      selectedNetworkUrl = selectedNetworkUrl.concat(network).concat(",");
+
+    }
+
+    this.getNearestLocationInfoService.getStationInfoFromSearchedLoc(selectedNetworkUrl).subscribe((info) => {
+      this.response = info;
+
+      this.response.fuel_stations.forEach((station: FuelStationInfo) => {
+
+        /*console.log('Station Name:', station.station_name,
+          '**Station network:', station.ev_network,
+          '**Station connector type:', station.ev_connector_types,
+          '**Latitude', station.latitude,
+          '**Longitude', station.longitude, '\n');*/
+
+        let coord: Coordinate = { lat: station.latitude, lng: station.longitude };
+
+        let statObj: StationObj = {
+          stationId: station.id, stationLocCoord: coord, stationPhone: station.station_phone,
+          stationName: station.station_name, stationAddress: station.street_address, stationCity: station.city,
+          stationState: station.state, stationZip: station.zip, stationHrs: station.access_days_time,
+          stationPricing: station.ev_pricing, stationRenewableSource: station.ev_renewable_source,
+          stationDCNum: station.ev_dc_fast_num, stationL1Num: station.ev_level1_evse_num,
+          stationL2Num: station.ev_level2_evse_num, color: ""
+        };
+        statObj.color = this.setMarkerIcon(statObj)
+        this.coordsFromNetworkFilterFormSearchLoc.push(statObj);
+      })
+    }
+    );
+    console.log(this.coordsFromNetworkFilterFormSearchLoc);
+    this.stationsToBeMarked = this.coordsFromNetworkFilterFormSearchLoc;
+  }
+
   onConnectorDeSelect(item: any) {
 
     let deSelectConnectorUrl = '&ev_connector_type='.concat(item.item_text);
@@ -367,47 +506,13 @@ export class DisplayMapsComponent implements OnInit {
     this.clearMapForConnectorFilter()
   }
 
-  clearMapForConnectorFilter() {
-    this.coordsFromConnectorFilterFormSearchLoc = [];
-    this.curStateOfConnectorFilterFromSearchLoc = [];
-    this.sharedService.sendConnectorSelect(this.coordsFromConnectorFilterFormSearchLoc);
+  onNetworkSelectAll(items: any) {
 
-  }
+    let allNetworkUrl = '&ev_connector_type=all';
+    allNetworkUrl = this.locSearched.concat(allNetworkUrl);
+    console.log(allNetworkUrl)
 
-
-  onLevelSelect(item: any) {
-    let selectLevelUrl = '&ev_charging_level=';
-
-    switch (item.item_text) {
-
-      case 'Level 1':
-        selectLevelUrl = selectLevelUrl.concat('1');
-        this.curStateOfLevelFilterFromSearchLoc.push('1')
-        break;
-
-      case 'Level 2':
-        selectLevelUrl = selectLevelUrl.concat('2');
-        this.curStateOfLevelFilterFromSearchLoc.push('2')
-        break;
-
-      case 'DC Fast':
-        selectLevelUrl = selectLevelUrl.concat('dc_fast');
-        this.curStateOfLevelFilterFromSearchLoc.push('3')
-        break;
-
-      case 'Legacy Chargers':
-        selectLevelUrl = selectLevelUrl.concat('legacy');
-        this.curStateOfLevelFilterFromSearchLoc.push('legacy')
-        break;
-
-      default:
-        selectLevelUrl = '';
-    }
-
-    selectLevelUrl = this.locSearched.concat(selectLevelUrl);
-    console.log(selectLevelUrl)
-
-    this.getNearestLocationInfoService.getStationInfoFromSearchedLoc(selectLevelUrl).subscribe((info) => {
+    this.getNearestLocationInfoService.getStationInfoFromSearchedLoc(allNetworkUrl).subscribe((info) => {
       this.response = info;
 
       this.response.fuel_stations.forEach((station: FuelStationInfo) => {
@@ -427,139 +532,18 @@ export class DisplayMapsComponent implements OnInit {
           stationPricing: station.ev_pricing, stationRenewableSource: station.ev_renewable_source,
           stationDCNum: station.ev_dc_fast_num, stationL1Num: station.ev_level1_evse_num,
           stationL2Num: station.ev_level2_evse_num, color: ""
-        };
-        statObj.color = this.setMarkerIcon(statObj)
-        this.coordsFromLevelFilterFormSearchLoc.push(statObj);
-      })
-    });
-    console.log(this.coordsFromLevelFilterFormSearchLoc);
-    this.sharedService.sendLevelSelect(this.coordsFromLevelFilterFormSearchLoc);
-  }
-  setMarkerIcon(statObj: StationObj): string {
-
-    if (statObj.stationL2Num != null) statObj.color = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
-    else if (statObj.stationL1Num != null) statObj.color = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-    else if (statObj.stationDCNum != null) statObj.color = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
-    else statObj.color = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-
-    return statObj.color;
-  }
-  onLevelSelectAll(items: any) {
-    let allLevelurl = '&ev_charging_level=all';
-    allLevelurl = this.locSearched.concat(allLevelurl);
-    console.log(allLevelurl)
-
-    this.getNearestLocationInfoService.getStationInfoFromSearchedLoc(allLevelurl).subscribe((info) => {
-      this.response = info;
-
-      this.response.fuel_stations.forEach((station: FuelStationInfo) => {
-
-        console.log('Station Name:', station.station_name,
-          '**Station network:', station.ev_network,
-          '**Station connector type:', station.ev_connector_types,
-          '**Latitude', station.latitude,
-          '**Longitude', station.longitude, '\n');
-
-        let coord: Coordinate = { lat: station.latitude, lng: station.longitude };
-
-        let statObj: StationObj = {
-          stationId: station.id, stationLocCoord: coord, stationPhone: station.station_phone,
-          stationName: station.station_name, stationAddress: station.street_address, stationCity: station.city,
-          stationState: station.state, stationZip: station.zip, stationHrs: station.access_days_time,
-          stationPricing: station.ev_pricing, stationRenewableSource: station.ev_renewable_source,
-          stationDCNum: station.ev_dc_fast_num, stationL1Num: station.ev_level1_evse_num,
-          stationL2Num: station.ev_level2_evse_num, color: ""
         }
         statObj.color = this.setMarkerIcon(statObj)
-        this.coordsFromLevelFilterFormSearchLoc.push(statObj);
+        this.coordsFromNetworkFilterFormSearchLoc.push(statObj);
       }
       );
-      console.log(this.coordsFromLevelFilterFormSearchLoc);
-      this.sharedService.sendLevelSelect(this.coordsFromLevelFilterFormSearchLoc);
+      console.log(this.coordsFromNetworkFilterFormSearchLoc);
+      this.stationsToBeMarked = this.coordsFromNetworkFilterFormSearchLoc
     }
     );
     console.log(items);
   }
-  onLevelDeSelect(item: any) {
-    //  console.log('In deslect: ' + item.item_text)
-    //  console.log('Before switch exec: ' + this.curStateOfLevelFilter);
-    console.log('hereeee')
-    let deSelectLevelUrl = '&ev_charging_level=';
 
-    switch (item.item_text) {
-
-      case 'Level 1':
-        deSelectLevelUrl = deSelectLevelUrl.concat('1');
-        this.curStateOfLevelFilterFromSearchLoc = this.curStateOfLevelFilterFromSearchLoc.filter(e => e !== '1');
-        break;
-
-      case 'Level 2':
-        deSelectLevelUrl = deSelectLevelUrl.concat('2');
-        this.curStateOfLevelFilterFromSearchLoc = this.curStateOfLevelFilterFromSearchLoc.filter(e => e !== '2');
-        break;
-
-      case 'DC Fast':
-        deSelectLevelUrl = deSelectLevelUrl.concat('dc_fast');
-        this.curStateOfLevelFilterFromSearchLoc = this.curStateOfLevelFilterFromSearchLoc.filter(e => e !== 'dc_fast');
-        break;
-
-      case 'Legacy Chargers':
-        deSelectLevelUrl = deSelectLevelUrl.concat('legacy');
-        this.curStateOfLevelFilterFromSearchLoc = this.curStateOfLevelFilterFromSearchLoc.filter(e => e !== 'legacy');
-        break;
-    }
-
-    deSelectLevelUrl = this.locSearched.concat(deSelectLevelUrl);
-    console.log(deSelectLevelUrl)
-    //  console.log('After switch exec: ' + this.curStateOfLevelFilter);
-    if (!(this.curStateOfLevelFilterFromSearchLoc.length == 0)) {
-
-      console.log('String to be passed:Getting the coords to be removed ' + deSelectLevelUrl);
-      this.getNearestLocationInfoService.getStationInfoFromSearchedLoc(deSelectLevelUrl).subscribe((info) => {
-        this.response = info;
-
-        this.response.fuel_stations.forEach((station: FuelStationInfo) => {
-
-          console.log('Station Name:', station.station_name,
-            '**Station network:', station.ev_network,
-            '**Station connector type:', station.ev_connector_types,
-            '**Latitude', station.latitude,
-            '**Longitude', station.longitude, '\n');
-
-          let coord: Coordinate = { lat: station.latitude, lng: station.longitude };
-
-          console.log('Cood to be eliminated ');
-          console.log(coord)
-
-          let i = -1;
-          this.coordsFromLevelFilterFormSearchLoc.forEach(ord => {
-
-            i++;
-            if (ord.stationLocCoord.lat == coord.lat && ord.stationLocCoord.lng == coord.lng) {
-              console.log(ord);
-              this.coordsFromLevelFilterFormSearchLoc.splice(i, 1);
-            }
-
-          });
-          console.log('Final list after removal');
-          console.log(this.coordsFromLevelFilterFormSearchLoc)
-        }
-        )
-      });
-      this.sharedService.sendLevelSelect(this.coordsFromLevelFilterFormSearchLoc);
-    }
-    else {
-      this.clearMapForLevelFilter()
-    }
-  }
-  onLevelDeSelectAll(items: any) {
-    this.clearMapForLevelFilter();
-  }
-  clearMapForLevelFilter() {
-    this.coordsFromLevelFilterFormSearchLoc = [];
-    this.curStateOfLevelFilterFromSearchLoc = [];
-    this.sharedService.sendLevelSelect(this.coordsFromLevelFilterFormSearchLoc);
-  }
 
 
 }
